c4104254bb2f44ef67ef66a658f534c4d3772a60
Haytay42 <62945386+Haytay42@users.noreply.github.com>
2021-11-15 19:49:25 +0300

    Создана общая фикстура для всех тестов

--- fixture/group.py
+++ fixture/group.py
@@ -28,7 +28,17 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
         wd.find_element_by_name("submit").click()
+        # submit group creation
         self.return_to_groups_page()
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_group_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+
 
