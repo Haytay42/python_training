ad7769e591837e7c099d9dfdd24338a84097f8e9
Haytay42 <62945386+Haytay42@users.noreply.github.com>
2021-12-23 18:03:56 +0300

    Доработан тест удаления контакта

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -3,7 +3,7 @@ import random
 
 
 def test_delete_first_contact(app, db, check_ui):
-    if app.contact.count() == 0:
+    if len(db.get_contact_list()) == 0:
         app.contact.create(Contact(firstname="Lisa", middlename="Sea", lastname="Isa", nickname="Hay", photo="C:\\Users\\Haytay\\PycharmProjects\\python_training\\Test.png", title="tit", company="comp", address="adr", home_telephone="898", mobile_telephone="893423422", work_telephone="898", fax_telephone="898", email="a@asd.ru", email2="saew@imnr.ru", email3="aw@inw.ru", homepage="aa", bday="1", bmonth="May", byear="2000", aday="3", amonth="May", ayear="2003", address2="1qwe", secondary_phone="89233", notes="1as2"))
     old_contacts = db.get_contact_list()
     contact = random.choice(old_contacts)
