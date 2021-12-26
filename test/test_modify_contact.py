3172bf3e6276c8e64ffc70ff012c52f8530b4b9e
Haytay42 <62945386+Haytay42@users.noreply.github.com>
2021-12-26 19:17:36 +0300

    Доработаны тесты модификации групп и контактов

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -6,7 +6,7 @@ from random import randrange
 def test_modify_contact(app, db, check_ui):
     if len(db.get_contact_list()) == 0:
         app.contact.create(Contact(firstname="Lisa", middlename="Sea", lastname="Isa", nickname="Hay", photo="C:\\Users\\Haytay\\PycharmProjects\\python_training\\Test.png", title="tit", company="comp", address="adr", home_telephone="898", mobile_telephone="893423422", work_telephone="898", fax_telephone="898", email="a@asd.ru", email2="saew@imnr.ru", email3="aw@inw.ru", homepage="aa", bday="1", bmonth="May", byear="2000", aday="3", amonth="May", ayear="2003", address2="1qwe", secondary_phone="89233", notes="1as2"))
-    old_contacts = app.contact.get_contact_list()
+    old_contacts = db.get_contact_list()
     index = randrange(len(old_contacts))
     contact = Contact(firstname="Lisa2", middlename="Sea2", lastname="Isa2", nickname="Hay2", photo="C:\\Users\\Haytay\\PycharmProjects\\python_training\\Test1.png", title="tit2", company="comp2", address="adr2", home_telephone="8982", mobile_telephone="8934234222", work_telephone="8982", fax_telephone="8982", email="a@asd.ru2", email2="saew@imnr.ru2", email3="aw@inw.ru2", homepage="aa2", bday="12", bmonth="August", byear="2001", aday="4", amonth="August", ayear="2004", address2="1qwe1", secondary_phone="892331", notes="1as2d")
     contact.id = old_contacts[index].id
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -6,7 +6,7 @@ from random import randrange
 def test_modify_group_name(app, db, check_ui):
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name='test'))
-    old_groups = app.group.get_group_list()
+    old_groups = db.get_group_list()
     index = randrange(len(old_groups))
     group = Group(name="New group")
     group.id = old_groups[index].id
