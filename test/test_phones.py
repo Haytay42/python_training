225bd91e33a549b4e58cc5dfca38cb650d7ace54
Haytay <schabnowalisa@mail.ru>
2022-01-08 14:08:14 +0300

    Реализован тест добавления группы в стиле BDD

--- test/test_phones.py
+++ test/test_phones.py
@@ -1,9 +1,14 @@
 import re
 
-def test_phones_on_home_page (app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+from model.contact import Contact
+
+
+def test_phones_on_home_page(app, db):
+    old_contacts = db.get_contact_list()
+    contact_from_home_page = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
+    contact_from_edit_page = sorted(db.get_contact_list(), key=Contact.id_or_max)
+    for i in range(len(old_contacts)):
+        assert contact_from_home_page[i].all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page[i])
 
 def test_phones_on_contact_view_page (app):
     contact_from_view_page = app.contact.get_contact_from_view_page(0)
